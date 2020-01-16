f6b30e6f46bb3c1b160a55f58b6fa617515316dc
mrys <1510218499@qq.com>
2020-01-16 17:44:13 +0800

    study

--- study/src/main/java/top/mrys/study/demo1/Demo1.java
+++ study/src/main/java/top/mrys/study/demo1/Demo1.java
@@ -1,8 +1,11 @@
 package top.mrys.study.demo1;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.lang.reflect.TypeVariable;
 import java.util.Arrays;
 
 /**
@@ -11,11 +14,18 @@ import java.util.Arrays;
  */
 @ComponentScan
 public class Demo1 {
+
+	private static Log log = LogFactory.getLog(Demo1.class);
+
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Demo1.class);
-		System.out.println("-----------------");
+		System.out.println("--------发的吉林省---------");
+		log.info("2123daf的方式多个");
 		String[] names = context.getBeanDefinitionNames();
 		Arrays.stream(names).forEach(System.out::println);
+		if (context instanceof TypeVariable) {
+			System.out.println(123123);
+		}
 		System.out.println("-----------------");
 	}
 }
